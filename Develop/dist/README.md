4ee42bc5ebf78934a62cab4b0ac2cbb2732e8193
dannyrgd <danny.ramirezgd@gmail.com>
2022-01-20 12:52:10 -0500

    markdown works with license badge creation

---
+++ Develop/dist/README.md
@@ -0,0 +1,29 @@
+
+  # Danny
+
+  ## Description
+  Danny
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  ## Installation
+  Danny
+  ## Usage
+
+
+  ## Contributing
+  If you are looking to contribute you can for example: 
+  * Danny
+
+  If you have any questions or comments you can reach out to me directly at:Danny
+    
+  ## License
+  Copyright (c) Danny
+
+  Licensed under the GPL-3.0 License
+  
+[![License: GPL-3.0](https://img.shields.io/badge/License-GPL_3.0-yellow.svg)](https://opensource.org/licenses/GPL-3.0)
+  
+  
--- Develop/index.js
+++ Develop/index.js
@@ -1,66 +1,81 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-const promptUser = () => { 
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'username',
-            message: 'What is your GitHub Username?'
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email address?'
-        },
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your project?'
-        }, 
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Please provide a short description of the project.'
-        }, 
-        {
-            type: 'input',
-            name: 'license',
-            message: 'What kind of license should your project have?',
-        },
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'What command should be run to install dependencies'
-        },
-        {
-            type: 'input',
-            name: 'tests',
-            message: 'What command should be run to run tests?'
-        },
-        {
-            type: 'input',
-            name: 'features',
-            message: 'What does the user need to know about using the Repo?'
-        },
-        {
-            type: 'input',
-            name: 'contributing',
-            message: 'What does the user need to know about contributing to the repo?'
-        }
-    ])
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "username",
+      message: "What is your GitHub Username?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?",
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please provide a short description of the project.",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What kind of license should your project have?",
+      choices: [
+        "MIT",
+        "Apache-2.0",
+        "GPL-2.0",
+        "GPL-3.0",
+        "EPL-2.0",
+        "MPL-2.0", 
+        "LGPL-2.0",
+        "LGPL-2.1",
+        "LGPL-3.0",
+        "BSD-2-Clause", 
+        "BSD-3-Clause",      
+        "Unlicense",
+      ],
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What command should be run to install dependencies",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "What command should be run to run tests?",
+    },
+    {
+      type: "input",
+      name: "features",
+      message: "What does the user need to know about using the Repo?",
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message:
+        "What does the user need to know about contributing to the repo?",
+    },
+  ]);
 };
-const writeToFile = fileName => {
-        return fs.writeFile("./dist/README.md", fileName, err => {
-            err ? console.error(err) : console.log('file created!')
-        }) 
-}
+const writeToFile = (fileName) => {
+  return fs.writeFile("./dist/README.md", fileName, (err) => {
+    err ? console.error(err) : console.log("file created!");
+  });
+};
 promptUser()
-.then(data => {
-    return generateMarkdown(data)
-})
-.then(markdownData => {
-    return writeToFile(markdownData)
-});
-
+  .then((data) => {
+      console.log(data.username);
+    return generateMarkdown(data);
+  })
+  .then((markdownData) => {
+    return writeToFile(markdownData);
+  });
